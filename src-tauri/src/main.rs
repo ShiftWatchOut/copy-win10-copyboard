24a8d877513edf05b6332650709592be7f03d8af
shift <2080717464@qq.com>
2022-08-13 00:18:46 +0800

    feat: 添加系统托盘菜单

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,34 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let quit = CustomMenuItem::new("quit", "退出");
+    let hide_or_show = CustomMenuItem::new("hide_or_show", "隐藏/显示");
+    let tray_menu = SystemTrayMenu::new().add_item(hide_or_show).add_item(quit);
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide_or_show" => {
+                    let window = app.get_window("main").unwrap();
+                    let is_visible = window.is_visible().unwrap();
+                    if is_visible {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap()
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
