9664ef895bdbbf84a576b3cde1e4023a971b809e
watchout <2080717464@qq.com>
2022-08-17 09:42:56 +0800

    feat: 点击粘贴并隐藏窗口

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
 import './App.scss'
 import TitleBar from "./components/TitleBar";
 import CopyList from "./components/CopyList";
-import {useEffect} from "react";
-import {listen} from "@tauri-apps/api/event";
-import {invoke} from "@tauri-apps/api";
-
-let inited = false;
 
 function App() {
-  useEffect(() => {
-    let unlisten: any;
-    if (!inited) {
-      inited = true
-      invoke("plugin:clipboard-watch|watch").then(value => {
-        console.log('init then', value)
-      }).catch(error => {
-        console.log('init catch', error)
-      });
-      (async () => {
-        unlisten = await listen("hello-event", (event) => {
-          console.log('OHHHHHHHHHHHHHH', event.payload)
-        })
-      })()
-    }
-    return () => {
-      unlisten?.()
-    }
-  }, [])
   return (
     <div className="App">
       <TitleBar/>
